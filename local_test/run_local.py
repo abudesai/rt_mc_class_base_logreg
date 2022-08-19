ce396e5ff2bc81ea1c6598107d218ffe5859b605
Abu Desai <abhyuday.desai@gmail.com>
2022-08-19 11:09:38 -0700

    update to run_local

--- local_test/run_local.py
+++ local_test/run_local.py
@@ -140,63 +140,47 @@ def load_and_test_algo():
     # save predictions
     utils.save_dataframe(predictions, testing_outputs_path, "test_predictions.csv")
     # score the results
-    results = score(test_data, predictions)  
+    results = score(test_data, predictions, data_schema)  
     print("done with predictions")
     return results
 
 
-def set_scoring_vars(dataset_name):
-    global target_field
-    if dataset_name == "car": 
-        target_field = "class"
-    elif dataset_name == "primary_tumor": 
-        target_field = "class"
-    elif dataset_name == "splice": 
-        target_field = "Class"
-    elif dataset_name == "statlog": 
-        target_field = "class"
-    elif dataset_name == "steel_plate_fault": 
-        target_field = "target"
-    elif dataset_name == "wine": 
-        target_field = "Wine class"
-    else: raise Exception(f"Error: Cannot find dataset = {dataset_name}")
-
-
-
-def score(test_data, predictions): 
-    class_names = predictions.columns[1:]    
-    pred_classes = pd.DataFrame(predictions[class_names], columns = class_names).idxmax(axis=1) 
+def score(test_data, predictions, data_schema): 
+    # we need to get a couple of field names in the test_data file to do the scoring 
+    # we get it using the schema file
+    id_field = data_schema["inputDatasets"]["multiClassClassificationBaseMainInput"]["idField"]
+    target_field = data_schema["inputDatasets"]["multiClassClassificationBaseMainInput"]["targetField"]
+        
+    pred_class_names = [ c for c in predictions.columns[1:]    ]  
     
-    accu = accuracy_score(test_data[target_field], pred_classes)    
-    f1 = f1_score(test_data[target_field], pred_classes, average='weighted')    
-    precision = precision_score(test_data[target_field], pred_classes, average='weighted')    
-    recall = recall_score(test_data[target_field], pred_classes, average='weighted')
-    auc = calculate_auc(test_data, predictions)     
-    results = { 
+    predictions["__pred_class"] = pd.DataFrame(predictions[pred_class_names], columns = pred_class_names).idxmax(axis=1)  
+    predictions = predictions.merge(test_data[[id_field, target_field]], on=[id_field])
+    pred_probabilities = predictions[pred_class_names].copy()
+    
+    Y = predictions[target_field].astype(str)
+    Y_hat = predictions["__pred_class"].astype(str)    
+    
+    accu = accuracy_score(Y , Y_hat)  
+    f1 = f1_score(Y , Y_hat, average='weighted')    
+    precision = precision_score(Y , Y_hat, average='weighted')      
+    recall = recall_score(Y , Y_hat, average='weighted') 
+    # -------------------------------------
+    # auc calculation         
+    name_to_idx_dict = {str(n):i for i,n in enumerate(pred_class_names)}
+    mapped_classes_true = Y.map(name_to_idx_dict)     
+    
+    auc = roc_auc_score(mapped_classes_true, pred_probabilities[pred_class_names].values, 
+        labels=np.arange(len(pred_class_names)), average='weighted', multi_class='ovo')     
+    
+    # -------------------------------------   
+    scores = { 
                "accuracy": np.round(accu,4), 
                "f1_score": np.round(f1, 4), 
                "precision": np.round(precision, 4), 
                "recall": np.round(recall, 4), 
                "auc_score": np.round(auc, 4), 
                }
-    return results
-
-
-def calculate_auc(test_data, predictions):
-    pred_class_names = list(predictions.columns[1:] )
-    obs_class_names =  list(set(test_data[target_field]))    
-    # find classes in test data that were not observed in training data (if any)
-    missing_classes = [c for c in obs_class_names if c not in pred_class_names]
-    class_names = pred_class_names + missing_classes  
-    for c in missing_classes: 
-        predictions[c] = 0.0
-    
-    name_to_idx_dict = {n:i for i,n in enumerate(class_names)}
-    mapped_classes_true = test_data[target_field].map(name_to_idx_dict)     
-    auc = roc_auc_score(mapped_classes_true, predictions[class_names], 
-        labels=np.arange(len(class_names)), average='weighted', multi_class='ovo') 
-    
-    return auc
+    return scores
     
 
 
@@ -227,7 +211,6 @@ def run_train_and_test(dataset_name, run_hpt, num_hpt_trials):
     if run_hpt: run_HPT(num_hpt_trials)               # run HPT and save tuned hyperparameters
     train_and_save_algo()        # train the model and save
     
-    set_scoring_vars(dataset_name=dataset_name)
     results = load_and_test_algo()        # load the trained model and get predictions on test data
     
     end = time.time()
@@ -249,7 +232,7 @@ if __name__ == "__main__":
     
     num_hpt_trials = 10
     run_hpt_list = [False, True]
-    run_hpt_list = [True]
+    run_hpt_list = [False]
     
     datasets = ["car", "primary_tumor", "splice", "statlog", "steel_plate_fault", "wine"]
     datasets = ["car"]
